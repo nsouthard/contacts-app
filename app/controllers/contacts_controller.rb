1d49bad112b34188914d84375af4948ba2891a6f
nsouthard <nathan.southard@gmail.com>
2017-05-08 21:21:45 -0500

    End of night save 5/8/17

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,46 @@
 class ContactsController < ApplicationController
+  
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+
+  end
+
+  def create
+    contact = Contact.new(
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          email: params[:email],
+                          phone_number: params[:phone_number]
+                          )
+    contact.save
+  end
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    contact.assign_attributes(
+                              first_name: params[:first_name],
+                              last_name: params[:last_name],
+                              email: params[:email],
+                              phone_number: params[:phone_number]
+                              )
+    contact.save
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+  end
+  
 end
