891691658d75d450214a88a3204f32658ed2621e
nsouthard <nathan.southard@gmail.com>
2017-05-15 21:22:29 -0500

    End of night save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create!([
+  {first_name: "Nathan", last_name: "Southard", email: "nathan.southard@gmail.com", phone_number: "312-804-4190", middle_name: nil, bio: nil},
+  {first_name: "Karol", last_name: "Gurba", email: "KarolGurba@gmail.com", phone_number: "773-540-9324", middle_name: nil, bio: nil},
+  {first_name: "Zoe", last_name: "Lembeck", email: "zoe.lembeck@gmail.com", phone_number: "773-540-9324", middle_name: "Hillary", bio: nil},
+  {first_name: "Brittany", last_name: "Wetzel", email: "BrittanyWetzel@gmail.com", phone_number: "773-555-7448", middle_name: "Sarah", bio: "Hair don't care"}
+])
